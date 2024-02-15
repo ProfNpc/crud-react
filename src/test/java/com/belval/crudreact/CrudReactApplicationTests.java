a1783c37e3f698ee54d6a7a2d3dd91bb3020b882
Alessandro <professornpc@gmail.com>
2024-02-15 20:38:39 -0300

    Versão inicial

---
+++ src/test/java/com/belval/crudreact/CrudReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belval.crudreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
